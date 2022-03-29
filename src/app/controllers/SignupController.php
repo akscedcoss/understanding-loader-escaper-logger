f602e22cb6723eed9ce3f9a7dfbc2d7544adbff0
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-03-29 12:50:41 +0530

    sanatize added to signup

--- src/app/controllers/SignupController.php
+++ src/app/controllers/SignupController.php
@@ -1,7 +1,7 @@
 <?php
 
 use Phalcon\Mvc\Controller;
-
+use \App\Components\myescaper;
 class SignupController extends Controller
 {
 
@@ -15,11 +15,20 @@ class SignupController extends Controller
 
     public function registerAction()
     {
-        $user = new Users();
+       
         // print_r( $this->request->getPost()) ;
         // die();
+        $helper= new myescaper();
+        // echo $helper->sanitize('dfbdf');
+
+        $data=[
+            'name'=>$helper->sanitize($this->request->getPost()['name']),
+            'email'=>$helper->sanitize($this->request->getPost()['email']),
+            'password'=>$helper->sanitize($this->request->getPost()['password']),
+        ];
+        $user= new Users();
         $user->assign(
-            $this->request->getPost(),
+            $data,
             [
                 'name',
                 'email',
